6ed5b4297a13ed4ef9d7700954247ac7270c5dc1
felix tineo <ftandcompany@gmail.com>
2020-10-27 16:52:47 -0400

    correciones

--- src/_sections/about/team.js
+++ src/_sections/about/team.js
@@ -67,7 +67,7 @@ export default ()=> {
         <Row>
           <Col xs={12}>
             <Title>
-              ¿Quienes somos?
+              ¿Quiénes somos?
             </Title>
           </Col>
           {
--- src/_sections/home/about.js
+++ src/_sections/home/about.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { Row, Col, Container, Hidden } from 'react-grid-system';
 import { Button } from '../../_components/buttons';
 import { ServiceCarousel, ReviewCarousel } from '../../_components/carousels';
+import { navigate } from 'gatsby';
 
 const MainCont = styled.section`  
   position: relative;
@@ -93,7 +94,7 @@ export default ()=> {
           <Description>
             {state.about.banner.subTitle}
           </Description>
-          <Button primary>
+          <Button primary onClick={()=> navigate("/about")}>
             {state.about.banner.buttonText}
           </Button>
         </DescriptionCont>        
